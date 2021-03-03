63116749cb550fb463309fcb5765af8cff460511
tabcode <tabcodet@gmail.com>
2021-03-02 21:55:06 -0600

    ok

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://root:root@cluster0.pnst7.mongodb.net/GRAPHQL?retryWrites=true&w=majority', {
+mongoose.connect('#', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
